7331f15943ebeffe7a780f9b1329e0c4fe22ea67
saidugurlu <ugurlumsaid@gmx.de>
2022-06-02 11:38:06 +0200

    both pages show books

--- src/pages/PageGeneralBooks.jsx
+++ src/pages/PageGeneralBooks.jsx
@@ -1,8 +1,13 @@
-export const PageGeneralBooks = ({generalBooks}) => {
+export const PageGeneralBooks = ({ generalBooks }) => {
 	return (
 		<div className="page_generalBooks">
 			<h2>General Books</h2>
-			<p>Here sre the {generalBooks.length} tech books.</p>
+			<p>Here are the {generalBooks.length} general books:</p>
+			<ul>
+				{generalBooks.map((book, index) => {
+					return <li key={index}>{book.title}</li>;
+				})}
+			</ul>
 		</div>
-	)
-}
+	);
+};
--- src/pages/PageTechBooks.jsx
+++ src/pages/PageTechBooks.jsx
@@ -2,7 +2,12 @@ export const PageTechBooks = ({techBooks}) => {
 	return (
 		<div className="page_techBooks">
 			<h2>Tech Books</h2>
-			<p>Here sre the {techBooks.length} tech books.</p>
+			<p>Here are the {techBooks.length} tech books.</p>
+			<ul>
+				{techBooks.map((book, index) => {
+					return <li key={index}>{book.title}</li>;
+				})}
+			</ul>
 		</div>
 	)
 }
