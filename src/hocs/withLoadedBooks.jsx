bdbbf16aad3833bd1ede3a51f4bc90cea9d4fc53
saidugurlu <ugurlumsaid@gmx.de>
2022-06-02 11:13:46 +0200

    bokks are loaded

--- src/hocs/withLoadedBooks.jsx
+++ src/hocs/withLoadedBooks.jsx
@@ -4,7 +4,21 @@ import axios from 'axios';
 const techBooksUrl = 'https://edwardtanguay.netlify.app/share/techBooks.json';
 const generalBooksUrl = 'https://edwardtanguay.netlify.app/share/books.json';
 
+return (props) => {
+    const [techBooks, setTechBooks] = useState([]);
+    const [generalBooks, setGeneralBooks] = useState([]);
 
+    useEffect(() => {
+        (async () => {
+            setTechBooks((await axios.get(techBooksUrl)).data);
+        })();
+    }, []);
+
+    useEffect(() => {
+        (async () => {
+            setGeneralBooks((await axios.get(generalBooksUrl)).data);
+        })();
+    }, []);
 
 const withLoadedBooks = () => {
 
